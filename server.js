c0088ab715ca86213f334f71499bd09956243c02
surbhit14 <surbhitagrawal716@gmail.com>
2025-07-31 20:15:01 +0530

    Resolve cors

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
